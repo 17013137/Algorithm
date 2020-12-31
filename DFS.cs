307855678d985c52ea79b1aad0062c9958b6040c
김동언 <gyry14pl@naver.com>
2020-12-31 11:46:02 +0900

    Create DFS.cs
    dfs

---
+++ DFS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6] {
+            {0, 1, 0, 1, 0, 0 },
+            {1, 0, 1, 1, 0, 0 },
+            {0, 1, 0, 0, 0, 0 },
+            {1, 1, 0, 0, 1, 0 },
+            {0, 0, 0, 1, 0, 1 },
+            {0, 0, 0, 0, 1, 0 }
+        };
+
+        List<int>[] adj2 = new List<int>[]
+        {
+            new List<int>() { 1, 3 },
+            new List<int>() { 0, 2, 3 },
+            new List<int>() { 1 },
+            new List<int>() { 0, 1, 4 },
+            new List<int>() { 3, 5 },
+            new List<int>() { 4 }
+        };
+        bool[] visited = new bool[6];
+        // 1) 우선 now부터 방문하고.
+        // 2) now와 연결된 정점들을 하나씩 확인해서, 아직 미발견(미방문) 상태라면 방문한다.
+      
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+            graph.DFS(5);
+
+        }
+    }
+}
