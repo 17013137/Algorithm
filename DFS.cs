409fa3232630204182d05210de0d441d0c07f37e
17013137 <57669142+17013137@users.noreply.github.com>
2020-12-31 11:58:08 +0900

    Update DFS.cs

--- DFS.cs
+++ DFS.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,7 +29,41 @@ namespace Exercise
         bool[] visited = new bool[6];
         // 1) 우선 now부터 방문하고.
         // 2) now와 연결된 정점들을 하나씩 확인해서, 아직 미발견(미방문) 상태라면 방문한다.
-      
+        public void DFS(int now)
+        {
+            Console.WriteLine(now);
+            visited[now] = true; // 1) 우선 now부터 방문하고,
+
+            for(int next = 0 ; next < 6; next++)
+            {
+                if (adj[now, next] == 0) continue; // 연결되어 있지 않으면 스킵
+                if (visited[next]) continue; // 이미 방문했으면 스킵.
+                DFS(next);
+            }
+        }
+
+        public void DFS2(int now)
+        {
+            Console.WriteLine(now);
+            visited[now] = true;
+
+            foreach(int next in adj2[now])
+            {
+                if (visited[next] == true) continue;
+                DFS2(next); 
+            }
+        }
+
+        public void SearchAll()
+        {
+            visited = new bool[6];
+            for(int now = 0; now < 6; now++)
+            {
+                if (visited[now] == false)
+                    DFS(now);
+            }
+        }
+    }
 
     class Program
     {
